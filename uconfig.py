3dba7a6c3d1751be02c34b5c895fee6f845d836b
RWN-N <rowenbos@gmail.com>
2025-02-23 09:58:31 +0700

    fix ngrok forward host:port

--- uconfig.py
+++ uconfig.py
@@ -10,5 +10,5 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 NGROK_TOKEN = os.getenv("NGROK_AUTHTOKEN")
 
 if NGROK_TOKEN is not None:
-    listener = ngrok.forward(PORT, authtoken=NGROK_TOKEN)  # forwarding to localhost:PORT
-    print(f"Ingress established at: {listener.url()}")
+    listener = ngrok.forward(f"localhost:{PORT}", authtoken=NGROK_TOKEN)
+    print(f"\n\n----- Ingress established at: {listener.url()} -----\n\n")
